6c792adde65e5ed84c04af1eae057d9e881a0bc8
fedus <feduskolya@gmail.com>
2022-04-22 13:21:50 +0300

    Sample code

---
+++ src/store/reducers/userReducer.ts
@@ -0,0 +1,48 @@
+
+interface userState {
+   users: any[];
+   loading: boolean;
+   error: null | string
+}
+
+enum UserActionTypes {
+   FETCH_USERS = 'FETCH_USERS',
+   FETCH_USERS_SUCCES = 'FETCH_USERS_SUCCES',
+   FETCH_USERS_ERROR = 'FETCH_USERS_ERROR'
+}
+
+
+interface FetchUserAction {
+   type: UserActionTypes.FETCH_USERS
+}
+
+interface FetchUserSuccesAction {
+   type: UserActionTypes.FETCH_USERS_SUCCES,
+   payload: any[]
+}
+
+interface FetchUserErrorAction {
+   type: UserActionTypes.FETCH_USERS_ERROR,
+   payload: null | string
+}
+
+type UserAction = FetchUserAction | FetchUserSuccesAction | FetchUserErrorAction
+
+const initialState: userState = {
+   users: [],
+   loading: false,
+   error: null,
+}
+
+export const userRedicer = (state = initialState, action: UserAction) => {
+   switch (action.type) {
+      case UserActionTypes.FETCH_USERS:
+         return state
+      case UserActionTypes.FETCH_USERS_SUCCES:
+         return state
+      case UserActionTypes.FETCH_USERS_ERROR:
+         return state
+      default:
+         return state
+   }
+}
